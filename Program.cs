fbc57bf15128b75d7c0b1ae1dd4c84c1cfb8ae5f
AlexFordnirs <71636706+AlexFordnirs@users.noreply.github.com>
2021-11-08 10:59:41 +0200

    Add files via upload
    Добавленна выборка 1 числа

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("КАЛЬКУЛЯТОР");
+
+            Console.WriteLine("Введите первое число:");
+            int nam1=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите первое число:");
+            int nam2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите знак:");
+            string s = Console.ReadLine();
+
+            if (s == "+")
+            {
+                Console.WriteLine("Вывод:",nam1 + nam2);
+            }
+            else if (s == "-") 
+            {
+
+                Console.WriteLine("Вывод:", nam1 - nam2);
+            }
+        }
+    }
+}
